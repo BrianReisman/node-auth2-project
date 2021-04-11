2be7caa15e810251beab86ade6d58f3981009419
BrianReisman <brianreisman@protonmail.com>
2021-04-11 13:52:33 -0400

    login and register working

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -7,6 +7,10 @@ function find() {
 }
 
 function findBy(filter) {
+  return db("users")
+  .join("roles", "users.role_id", "roles.role_id")
+  .select("user_id", "username", "role_name", 'users.password')
+  .where(filter);
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users that match the filter condition.
